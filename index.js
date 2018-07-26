1b87f60a1511e5a2f142b30996bc4cc7cd29e97e
nenad83 <39170284+nenad83@users.noreply.github.com>
2018-07-26 21:33:06 +0200

    cas1

---
+++ index.js
@@ -0,0 +1,68 @@
+// console.log("Hello from node-js");
+
+// var integer1 = 5;
+// var integer2 = "10";
+// var sum = integer1+integer2;
+
+// console.log(integer1+integer2);
+// console.log(integer1 + "+ "+ integer2 + " =" + sum);
+
+
+// var name = "NENO";
+// var pass = "000000";
+// var nameUpdated = console.log(name.toLowerCase());
+// console.log(name.toLowerCase());
+
+
+// if(nameUpdated == "neno"){
+// 	console.log("Hello " + name);
+// }else {
+// 	console.log("Unsuccessful");
+// }
+
+// switch(name){
+// 	case "Neno":
+// 	console.log("Hello " + name);
+// 	break;
+// 	case "NENO":
+// 	console.log("Hello " + name);
+// 	break;
+// 	default:
+// 	console.log("Unsuccessful");
+// }
+
+// if (name == "Neno" || name == "iva"){
+// 	console.log("Hello " + name);
+// }else {
+// 	console.log("Unsuccessful");
+// }
+
+
+// var firstname = "Nenad ";
+// var lastname = "Milanovic";
+// var fullname = firstname.concat(lastname); ////////concat
+// console.log(fullname);
+
+
+// var boolean = true;
+// console.log(boolean);
+
+
+var integer = [3, 4, 5, 6, 7, 8, 9, 10, 12];
+
+
+integer[100] = 50;
+console.log(integer);
+
+// for(let i =0; i<integer.length; i++ ){
+// 	console.log(integer[i] + ": ")
+// 	if(integer[i]%3 == 0){
+// 		console.log("buzz");
+// 	}
+// 	if(integer[i] % 4 == 0){
+// 		console.log("fiz");
+// 	}
+
+// 	console.log("\n");
+// }
+
