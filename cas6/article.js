ed605948cfd48f300d4b6f5c70f783c9a56c7af3
nenad83 <39170284+nenad83@users.noreply.github.com>
2018-08-06 22:07:40 +0200

    cas 6

---
+++ cas6/article.js
@@ -0,0 +1,9 @@
+//Prototype za article object
+
+exports.create = function(id, title, content) {
+	this.id = id;
+	this.title = title;
+	this.content = content;
+
+
+}
---
+++ cas6/user.js
@@ -0,0 +1,7 @@
+exports.create = function(firstName,lastName, email, age ) {
+
+	this.firstName = firstName;
+	this.lastName = lastName;
+	this.email = email;
+	this.age = age;
+}
